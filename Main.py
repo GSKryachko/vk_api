3cf8c62955cb1049a773323d8671b69e54b39075
Eien_g <32865043+GrigoryEien@users.noreply.github.com>
2018-02-26 12:17:23 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Returns list of followers of requested user')
 parser.add_argument('user_id', type=int, help='User id')
-parser.add_argument('token', type=str, help='Access token', nargs='?')
+parser.add_argument('token', type=str, help='Access token')
 parser.add_argument('count', type=int, help='Number of users to return', nargs='?', default=100)
 args = parser.parse_args()
 
